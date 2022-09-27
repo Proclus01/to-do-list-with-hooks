31d4a24942d678cb7b7d566471623f66113ce0c3
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-26 21:42:17 -0700

    redo spacing

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import TodoList from './TodoList';
+import TodoList from "./TodoList";
 import { Typography } from "@material-ui/core";
 import { Paper } from "@material-ui/core";
 import { AppBar } from "@material-ui/core";
@@ -7,33 +7,32 @@ import { Toolbar } from "@material-ui/core";
 import { Grid } from "@material-ui/core";
 
 function TodoApp() {
+  const initialTodos = [
+    { id: 1, task: "Clean Fishtank", completed: false },
+    { id: 2, task: "Wash Car", completed: true },
+    { id: 3, task: "Grow Beard", completed: false },
+  ];
 
-    const initialTodos = [
-        { id: 1, task: "Clean Fishtank", completed: false },
-        { id: 2, task: "Wash Car", completed: true },
-        { id: 3, task: "Grow Beard", completed: false },
-    ];
+  const [todos, setTodos] = useState(initialTodos);
 
-    const [todos, setTodos] = useState(initialTodos);
-
-    return (
-        <Paper
-        style={{
-            padding: 0,
-            margin: 0,
-            height: "100vh",
-            backgroundColor: "#fafafa",
-        }}
-        elevation={0}
-        >
-            <AppBar color="primary" position="static" style={{ height: "64px" }}>
-                <Toolbar>
-                <Typography color="inherit">TO DOS WITH HOOKS</Typography>
-                </Toolbar>
-            </AppBar>
-            <TodoList todos={todos} />
-        </Paper>
-    );
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "#fafafa",
+      }}
+      elevation={0}
+    >
+      <AppBar color="primary" position="static" style={{ height: "64px" }}>
+        <Toolbar>
+          <Typography color="inherit">TO DOS WITH HOOKS</Typography>
+        </Toolbar>
+      </AppBar>
+      <TodoList todos={todos} />
+    </Paper>
+  );
 }
 
 export default TodoApp;

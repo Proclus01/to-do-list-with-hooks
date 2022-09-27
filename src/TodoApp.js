4f727bbd513a97064f38ce507e2cb6d5ae3f7b5c
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-26 21:35:16 -0700

    set up initial state in todo app component and set up boilerplate for todolist component

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+import TodoList from './TodoList';
 import { Typography } from "@material-ui/core";
 import { Paper } from "@material-ui/core";
 import { AppBar } from "@material-ui/core";
@@ -5,24 +7,32 @@ import { Toolbar } from "@material-ui/core";
 import { Grid } from "@material-ui/core";
 
 function TodoApp() {
-  return (
-    <Paper
+
+    const initialTodos = [
+        { id: 1, task: "Clean Fishtank", completed: false },
+        { id: 2, task: "Wash Car", completed: true },
+        { id: 3, task: "Grow Beard", completed: false },
+    ];
+
+    const [todos, setTodos] = useState(initialTodos);
+
+    return (
+        <Paper
         style={{
             padding: 0,
             margin: 0,
             height: "100vh",
-            backgroundColor: "#fafafa"
+            backgroundColor: "#fafafa",
         }}
         elevation={0}
-    >
-        <AppBar color='primary' position='static' style={{ height: "64px" }}>
-            <Toolbar>
-                <Typography color='inherit'>
-                    TO DOS WITH HOOKS
-                </Typography>
-            </Toolbar>
-        </AppBar>
-    </Paper>
+        >
+            <AppBar color="primary" position="static" style={{ height: "64px" }}>
+                <Toolbar>
+                <Typography color="inherit">TO DOS WITH HOOKS</Typography>
+                </Toolbar>
+            </AppBar>
+            <TodoList todos={todos} />
+        </Paper>
     );
 }
 
---
+++ src/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function TodoList(props) {
+    return (
+        <div>
+            {props.todos.map(
+                todo => (
+                    <li>{todo.task}</li>
+                )
+            )}
+        </div>
+    )   
+}
+
+export default TodoList;

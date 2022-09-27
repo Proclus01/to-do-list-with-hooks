cf70aa6c2f90527ba16d4c24c9bd2ff69f0ca07c
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-26 23:14:28 -0700

    set up useinput state to track state inside of todoform

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,11 +1,13 @@
-import { useInputState } from './hooks/useInputState';
+import useInputState from './hooks/useInputState';
 import { Paper } from "@material-ui/core";
 import { TextField } from "@material-ui/core";
 
 function TodoForm(props) {
+    const [value, handleChange, reset] = useInputState("");
+
     return (
       <Paper>
-        <TextField />
+        <TextField value={value} onChange={handleChange}/>
       </Paper>
     );
   }

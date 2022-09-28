bdf0ece2c34aaeae72e1f9d181ba197598e92b30
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-27 22:36:47 -0700

    add styling to mui components inside of todo

--- src/Todo.js
+++ src/Todo.js
@@ -4,18 +4,23 @@ import { ListItemText } from "@material-ui/core";
 import { Checkbox } from "@material-ui/core";
 import { Delete } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
+import { Edit}  from '@mui/icons-material';
 import { ListItemSecondaryAction } from "@material-ui/core";
 
 function Todo({task, completed}) {
     return (
         <ListItem>
             <Checkbox tabIndex={-1} checked={completed} />
-            <ListItemText>
+            <ListItemText style={{textDecoration: completed ? "line-through" : "none"}}>
                 {task}
             </ListItemText>
+
             <ListItemSecondaryAction>
-                <IconButton>
+                <IconButton aria-label="Delete">
                     <Delete />
+                </IconButton>
+                <IconButton aria-label="Edit">
+                    <Edit />
                 </IconButton>
             </ListItemSecondaryAction>
         </ListItem>
